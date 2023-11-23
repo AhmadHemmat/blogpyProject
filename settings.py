af7de45260e1e8248599ecae1ff6ea4287d3d5a5
Ahmad_Hemmati_Zoleyva <62217505+AhmadHemmat@users.noreply.github.com>
2023-11-23 11:15:18 +0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Base url to serve media files
+MEDIA_URL = '/media/'
 
+# Path where media is stored'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
